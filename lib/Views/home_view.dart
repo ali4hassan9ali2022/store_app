75c1fe8f4d1682fe55f26c7630faa8f5e4d5adc3
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-11 15:22:50 +0200

    create app bar to home view

--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
   static String id = "HomeView";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          "New Trend",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.cartPlus,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
